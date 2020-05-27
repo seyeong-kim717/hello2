310c8fa811f740b4e6a7cbbdb71aa6032b5ed315
seo <sykim1123@nate.com>
2020-05-27 18:10:18 +0900

    Step02_DataType 프로젝트 입니다.

---
+++ Step02_DataType/src/test/main/MainClass02.java
@@ -0,0 +1,16 @@
+package test.main;
+
+public class MainClass02 {
+	public static void main(String[] args) {
+		//국어 점수
+		int kor=95;
+		//영어점수
+		int eng=100;
+		
+		//국어점수와 영어점수의 평균을 구해서 변수를 담고 그결과를 콘솔창에 출력해 보세요.
+		
+		//int type과 double type를 연산하면 결과는 double type이다.
+		double avg = (kor+eng)/2.0;
+		System.out.println("평균:"+avg);
+	}
+}
---
+++ Step02_DataType/src/test/main/MainClass07.java
@@ -0,0 +1,13 @@
+package test.main;
+
+public class MainClass07 {
+	public static void main(String[] args) {
+		System.out.println("main 메소드가 시작 되었습니다.");
+		//지역변수를 만들고 값을 나중에 넣고 시으면 초기값을 대입하는 것이 좋다.
+		int num1=0;
+		//참조 데이터 type이 들어갈 빈 변수는 null을 대입해서 만든다.
+		String name1=null;
+		System.out.println("main 메소드가 종료 됩니다.");
+		//boolean 타입의 초기값은 false로  대입.
+	}
+}
